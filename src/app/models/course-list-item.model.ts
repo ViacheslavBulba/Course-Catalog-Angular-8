32573dfb7c7897b7aed0ce0d716069dd91be4747
ViacheslavBulba <MEGADEVICES@GMAIL.COM>
2019-10-13 21:03:54 -0500

    added models for user and course list item

---
+++ src/app/models/course-list-item.model.ts
@@ -0,0 +1,10 @@
+import { User } from './user.model';
+
+export interface CourseListItem {
+  id: number;
+  title: string;
+  description: string;
+  creationDate: Date;
+  durationInMinutes: number;
+  authors: Set<User>;
+}
