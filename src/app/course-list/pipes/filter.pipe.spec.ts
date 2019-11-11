760f8e5bc3bb5f3a46e2c1eabc61dc19d6392124
ViacheslavBulba <MEGADEVICES@GMAIL.COM>
2019-11-10 21:18:48 -0600

    pipes unit tests

--- src/app/course-list/pipes/filter.pipe.spec.ts
+++ src/app/course-list/pipes/filter.pipe.spec.ts
@@ -5,4 +5,30 @@ describe('FilterPipe', () => {
     const pipe = new FilterPipe();
     expect(pipe).toBeTruthy();
   });
+
+  it('filters array by title', () => {
+    const pipe = new FilterPipe();
+    const beforeFiltering = [
+      {
+        title: 'aaa',
+        creationDate: new Date('November 1 2019')
+      },
+      {
+        title: 'bbb',
+        creationDate: new Date('December 11 2019')
+      },
+      {
+        title: 'ccc',
+        creationDate: new Date('October 13 2019')
+      }
+    ];
+    const afterFiltering = [
+      {
+        title: 'bbb',
+        creationDate: new Date('December 11 2019')
+      }
+    ];
+    expect(pipe.transform(beforeFiltering, 'bbb')).toEqual(afterFiltering);
+  });
+
 });
--- src/app/course-list/pipes/order-by.pipe.spec.ts
+++ src/app/course-list/pipes/order-by.pipe.spec.ts
@@ -5,4 +5,38 @@ describe('OrderByPipe', () => {
     const pipe = new OrderByPipe();
     expect(pipe).toBeTruthy();
   });
+
+  it('orders array by creation date desc', () => {
+    const pipe = new OrderByPipe();
+    const beforeOrdering = [
+      {
+        title: 'course #2',
+        creationDate: new Date('November 1 2019')
+      },
+      {
+        title: 'course #1',
+        creationDate: new Date('December 11 2019')
+      },
+      {
+        title: 'course #3',
+        creationDate: new Date('October 13 2019')
+      }
+    ];
+    const afterOrdering = [
+      {
+        title: 'course #1',
+        creationDate: new Date('December 11 2019')
+      },
+      {
+        title: 'course #2',
+        creationDate: new Date('November 1 2019')
+      },
+      {
+        title: 'course #3',
+        creationDate: new Date('October 13 2019')
+      }
+    ];
+    expect(pipe.transform(beforeOrdering, '-creationDate')).toEqual(afterOrdering);
+  });
+
 });
