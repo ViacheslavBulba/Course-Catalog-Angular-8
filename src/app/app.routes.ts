d41edc123726724b43d0f9f56b4bf2db3193ebd1
ViacheslavBulba <MEGADEVICES@GMAIL.COM>
2019-12-01 20:36:48 -0600

    added page not found component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { CourseListComponent } from './course-list/course-list/course-list.compo
 import { LoginComponent } from './login/login/login.component';
 import { CourseDetailsComponent } from './course-list/course-details/course-details.component';
 import { AuthGuard } from './login/auth.guard';
+import { PageNotFoundComponent } from './layout/page-not-found/page-not-found.component';
 
 export const ROUTES: Route[] = [
   { path: '', component: CourseListComponent, canActivate: [AuthGuard] },
@@ -10,5 +11,5 @@ export const ROUTES: Route[] = [
   { path: 'login', component: LoginComponent },
   { path: 'courses/new', component: CourseDetailsComponent, canActivate: [AuthGuard] },
   { path: 'courses/:id', component: CourseDetailsComponent, canActivate: [AuthGuard] },
-  { path: '**', redirectTo: '' }
+  { path: '**', component: PageNotFoundComponent }
 ];
--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent, BreadcrumbsComponent],
+  declarations: [HeaderComponent, FooterComponent, BreadcrumbsComponent, PageNotFoundComponent],
   imports: [
     CommonModule
   ],
