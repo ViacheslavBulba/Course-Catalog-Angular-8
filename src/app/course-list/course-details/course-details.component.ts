b64c93c5b85c47afa9daa821476c05f27deab9d5
tolgaozkan <tolgaozkan@msn.com>
2019-12-10 11:35:58 +0100

    author fix

--- src/app/course-list/course-details/course-details.component.ts
+++ src/app/course-list/course-details/course-details.component.ts
@@ -1,19 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-import { CourseListItem } from 'src/app/models/course-list-item.model';
-import { Router, ActivatedRoute } from '@angular/router';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { BehaviorSubject } from 'rxjs';
 import { Author } from 'src/app/models/author.model';
+import { CourseListItem } from 'src/app/models/course-list-item.model';
+
 import { CoursesService } from '../services/courses.service';
 
 @Component({
   selector: 'app-course-details',
   templateUrl: './course-details.component.html',
-  styleUrls: ['./course-details.component.css']
+  styleUrls: ['./course-details.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CourseDetailsComponent implements OnInit {
 
-  header = 'New Course';
+  public header = 'New Course';
 
-  course: CourseListItem;
+  public course$ = new BehaviorSubject<CourseListItem>({
+    id: Number(new Date()),
+    title: '',
+    description: '',
+    creationDate: null,
+    durationInMinutes: null,
+    authors: null,
+    topRated: false
+  });
 
   private tempDuration: number = null;
 
@@ -21,28 +32,25 @@ export class CourseDetailsComponent implements OnInit {
 
   private tempAuthors: Set<Author> = null;
 
-  constructor(private router: Router, private coursesService: CoursesService, private activatedRoute: ActivatedRoute) {
+  constructor(
+    private router: Router,
+    private coursesService: CoursesService,
+    private activatedRoute: ActivatedRoute) {
   }
 
   ngOnInit() {
-    if (this.activatedRoute.snapshot.url.toString() !== 'new') {
-      this.header = 'Edit Course';
-      const id = Number(this.activatedRoute.snapshot.url);
-      this.coursesService.getCourseById(id).subscribe(course => {
-        this.course = course;
-        console.log(this.course);
-      });
-    } else {
-      this.course = {
-        id: Number(new Date()),
-        title: '',
-        description: '',
-        creationDate: null,
-        durationInMinutes: null,
-        authors: null,
-        topRated: false
-      };
+    if (this.activatedRoute.snapshot.url.toString() === 'new') {
+      return;
     }
+
+    this.header = 'Edit Course';
+
+    const id = Number(this.activatedRoute.snapshot.url);
+    this.coursesService
+      .getCourseById(id)
+      .subscribe(course => {
+        this.course$.next(course);
+      });
   }
 
   onCancel() {
@@ -61,47 +69,49 @@ export class CourseDetailsComponent implements OnInit {
     this.tempAuthors = users;
   }
 
-  onSave() {
+  onSave(course: CourseListItem) {
 
     if (this.tempDuration !== null) {
-      this.course.durationInMinutes = this.tempDuration;
+      course.durationInMinutes = this.tempDuration;
     }
     if (this.tempDate !== null) {
-      this.course.creationDate = this.tempDate;
+      course.creationDate = this.tempDate;
     }
     if (this.tempAuthors !== null) {
-      this.course.authors = this.tempAuthors;
+      course.authors = this.tempAuthors;
     }
 
     if (this.activatedRoute.snapshot.url.toString() !== 'new') {
       console.log('edit existing course');
-      this.coursesService.updateItem(this.course);
+      this.coursesService.updateItem(course);
     } else {
       console.log('create new course');
-      if (this.course.title === '') {
-        this.course.title = String(new Date());
+      if (course.title === '') {
+        course.title = String(new Date());
       }
-      if (this.course.description === '') {
-        this.course.description = 'Test course description';
+      if (course.description === '') {
+        course.description = 'Test course description';
       }
-      if (this.course.creationDate === null) {
-        this.course.creationDate = new Date();
+      if (course.creationDate === null) {
+        course.creationDate = new Date();
       }
-      if (this.course.durationInMinutes === null) {
-        this.course.durationInMinutes = 60;
+      if (course.durationInMinutes === null) {
+        course.durationInMinutes = 60;
       }
-      if (this.course.authors === null) {
-        this.course.authors = new Set<Author>([{
+      if (course.authors === null) {
+        course.authors = new Set<Author>([{
           id: 999,
           firstName: 'Auto',
           lastName: 'One'
         }]);
       }
-      this.coursesService.createCourse(this.course).subscribe((response) => {
-        console.log(response);
-      });
+      this.coursesService
+        .createCourse(course)
+        .subscribe((response) => {
+          console.log(response);
+          this.router.navigate(['/courses']);
+        });
     }
-    this.router.navigate(['/courses']);
   }
 
 }
