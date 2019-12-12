a43c0db0e729bd42bb44031c6dcf63480492d65b
tolgaozkan <tolgaozkan@msn.com>
2019-12-12 09:55:22 +0100

    date fix

--- src/app/course-list/date-selector/date-selector.component.ts
+++ src/app/course-list/date-selector/date-selector.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit, OnChanges, Input, ChangeDetectionStrategy, Output, EventEmitter } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-date-selector',
@@ -7,29 +6,13 @@ import { BehaviorSubject } from 'rxjs';
   styleUrls: ['./date-selector.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class DateSelectorComponent implements OnChanges {
+export class DateSelectorComponent {
 
-  public date$ = new BehaviorSubject<Date>(null);
-
-  public date: Date = null;
-
-  @Input() public dateInput: Date;
+  @Input() public date: Date;
   @Output() public dateOutput = new EventEmitter<Date>();
 
-  constructor() { }
-
-  ngOnChanges() {
-    this.date = this.dateInput;
-    this.date$.next(this.date);
-  }
-
-  saveDate(date: Date) {
-    console.log(date);
-    this.date = date;
-  }
-
   public emitOutput(event) {
-    this.dateOutput.emit(this.date);
+    this.dateOutput.emit(event.value);
   }
 
 }
