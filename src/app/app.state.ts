9b58502d9678525bf1624a46a6b966efd79eadbc
tolgaozkan <tolgaozkan@msn.com>
2020-01-04 22:15:57 +0100

    store design fix

---
+++ src/app/app.state.ts
@@ -0,0 +1,7 @@
+import { ActionReducerMap } from '@ngrx/store';
+
+// tslint:disable-next-line:no-empty-interface
+export interface AppState {
+}
+
+export const appReducer: ActionReducerMap<AppState> = {};
